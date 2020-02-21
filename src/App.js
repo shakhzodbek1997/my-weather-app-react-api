1f8f51046186e3f6ff83ac50218f4b4843265f61
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-02-21 11:47:02 +0500

    all temp parameters working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
 import './App.css';
+import 'weather-icons/css/weather-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from "./app_component/weather.component";
 
-function App() {
-  return (
-   <div className="App">
-       <Weather/>
-   </div>
-  );
+ const API_key = "5e91d25cc293bac259c3dcd2b3269b50";
+
+class App extends React.Component {
+constructor() {
+    super();
+    this.state = {
+        city: undefined,
+        country:undefined,
+        icon: undefined,
+        main: undefined,
+        celsius:undefined,
+        temp_max: undefined,
+        temp_min: undefined,
+        description:"",
+        error:false
+    };
+    this.getWeather();
+}
+
+calCelsius(temp){
+    let cell = Math.floor(temp-273.15);
+    return cell;
+}
+
+getWeather = async () => {
+   const api_call = await fetch(
+       `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+
+    const response = await api_call.json();
+
+    console.log(response);
+
+    this.setState({
+        city:response.name,
+        country: response.sys.country,
+        celsius:this.calCelsius(response.main.temp),
+        temp_max:this.calCelsius(response.main.temp_max),
+        temp_min:this.calCelsius(response.main.temp_min),
+    })
+
+};
+    render() {
+        return (
+            <div className="App">
+                <Weather
+                    city={this.state.city}
+                    country={this.state.country}
+                    temp_celsius={this.state.celsius}
+                    temp_max={this.state.temp_max}
+                    temp_min={this.state.temp_min}
+                    description={this.state.description}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 
-const Weather = () => {
+const Weather = (props ) => {
     return (
         <div className="container">
             <div className="cards">
-                <h1>London</h1>
+                <h1>
+                    {props.city},
+                    {props.country}
+                </h1>
+                <h5 className="py-4">
+                    <i className="wi wi-day-sunny display-1"></i>
+                </h5>
+                <h1 className="py-2">{props.temp_celsius}°</h1>
+                {/*show max and min temp*/}
+
+                {minMaxTemp(props.temp_min, props.temp_max)}
+
+                <h4 className="py-3">{props.description} </h4>
             </div>
         </div>
     );
 };
+
+function minMaxTemp(min, max) {
+    return(
+        <h3>
+            <span className="px-4">{min}°</span>
+            <span className="px-4">{max}°</span>
+        </h3>
+    )
+}
 
 export default Weather;
